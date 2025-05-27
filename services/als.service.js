6bb94fec43dbce9fb432c6f53327b99d1ca33fea
Boaz Teller <boaz.teller@gmail.com>
2025-05-27 14:36:55 +0300

    refactor: add getLoggedinUser to alsService

--- services/als.service.js
+++ services/als.service.js
@@ -1,4 +1,14 @@
+// async_hooks comes from Node.js core
 import { AsyncLocalStorage } from 'async_hooks'
 
 // The AsyncLocalStorage singleton
 export const asyncLocalStorage = new AsyncLocalStorage()
+
+export const alsService = {
+    getLoggedinUser
+}
+
+function getLoggedinUser() {
+    const store = asyncLocalStorage.getStore()
+    return store?.loggedinUser
+}
