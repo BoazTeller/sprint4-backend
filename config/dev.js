36e5aff9b33d5ca62b9563978d9a4fc289fcfe75
BoazTeller <boaz.teller@gmail.com>
2025-07-02 19:08:21 +0300

    chore: update dev/prod configs

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,5 @@
 export default  {
   dbURL: 'mongodb://127.0.0.1:27017',
-  dbName : 'spotify_db'
+  dbName : 'spotify_db',
+  GENIUS_ACCESS_TOKEN: process.env.GENIUS_ACCESS_TOKEN
 }
--- config/prod.js
+++ config/prod.js
@@ -1,12 +1,5 @@
 export default {
-  dbURL: process.env.MONGO_URL || 'mongodb+srv://zangetsu923:12345@cluster0.oxdptc6.mongodb.net/',
-  dbName: process.env.DB_NAME || 'spotify_db',
+    dbURL: process.env.MONGO_URL,
+    dbName: process.env.DB_NAME,
+    GENIUS_ACCESS_TOKEN: process.env.GENIUS_ACCESS_TOKEN
 }
-
-// import dotenv from 'dotenv'
-// dotenv.config()
-
-// export default {
-//     dbURL: process.env.MONGO_URL || 'mongodb+srv://stavyaarbar:anistav12345@cluster0.uqvtq.mongodb.net/',
-//     dbName: process.env.DB_NAME || 'spotify_db'
-// }
